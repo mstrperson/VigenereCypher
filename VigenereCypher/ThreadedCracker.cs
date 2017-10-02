1d2c2b55c02d54b1d29f4b9d2482f72de101cb75
Jason Cox <coxj@vt.edu>
2017-10-02 11:03:11 -0600

    Added Comments to the solution.

--- VigenereCypher/ThreadedCracker.cs
+++ VigenereCypher/ThreadedCracker.cs
@@ -16,6 +16,10 @@ namespace VigenereCypher
 		protected TimeSpan PrevDuration;
 		protected readonly int ThreadCount = 8;
 
+		/// <summary>
+		/// Initializes a new instance of the <see cref="T:VigenereCypher.ThreadedCracker"/> class.
+		/// This object will be the Cracker that uses multiple threads.
+		/// </summary>
 		public ThreadedCracker()
 		{
 			StreamReader reader = new StreamReader("dictionary.txt");
@@ -25,121 +29,21 @@ namespace VigenereCypher
 				dictionary.Add(reader.ReadLine().Trim());
 			}
 
+			// use all available processors!
+			ThreadCount = Environment.ProcessorCount;
+			Console.WriteLine("Initializing Cracker with {0} Processors.", ThreadCount);
 			StartTime = new DateTime();
 			Duration = new TimeSpan();
 			PrevDuration = new TimeSpan();
 		}
 
+		/// <summary>
+		/// Crack the specified encryptedMessage with the given Key length. Using the number of CPUs available.
+		/// </summary>
+		/// <returns>Possible solutions for the cracked message indexed by their key[].</returns>
+		/// <param name="encryptedMessage">Encrypted message.</param>
+		/// <param name="keyLength">Key length.</param>
 		new public Dictionary<int[], String> Crack(String encryptedMessage, int keyLength)
-		{
-			if (keyLength <= 3) return VigenereCypher.Crack(encryptedMessage, keyLength);
-
-			StartTime = DateTime.Now;
-			Dictionary<int[], String> solutions = new Dictionary<int[], string>();
-
-
-			int[] key = new int[keyLength];
-			for (int i = 0; i < keyLength; i++)
-			{
-				key[i] = 0;
-			}
-			Console.WriteLine("Starting crack Process at:  {0}", StartTime.ToLongTimeString());
-			PrevDuration = new TimeSpan(0);
-			Duration = new TimeSpan(0);
-			int end = 0;
-			while (end == 0)
-			{
-
-				Duration = DateTime.Now - StartTime;
-				if (Duration - PrevDuration > TimeSpan.FromMinutes(1))
-				{
-					PrevDuration = Duration;
-					Console.WriteLine("{0} has elapsed.", Duration);
-					Console.Write("Attempting Current Key:  ");
-					PrintKey(key);
-					Console.WriteLine("___________________________________");
-				}
-
-				SubCrack(encryptedMessage, key, ref solutions);
-
-				for (int i = 0; i < 100; i++, end = IncrementKey(ref key) ? 0 : 1)
-				{
-					if (end == 1) break;
-				}
-
-				if (end == 1) break;
-
-			}
-
-			return solutions;
-		}
-
-
-		public void SubCrack(String encryptedMessage, int[] startingKey, ref Dictionary<int[], String> solutions)
-		{
-			Dictionary<int[], string> mysolutions = new Dictionary<int[], string>();
-			int[] akey = startingKey;
-			List<int[]> keys = new List<int[]>();
-			for (int i = 0; i < 100; i++, IncrementKey(ref akey))
-			{
-				keys.Add(akey);
-			}
-			Parallel.ForEach(keys, (key) =>
-			{
-				Console.Write("trying ");
-				PrintKey(key);
-				String message = DecryptMessage(encryptedMessage, key);
-				String[] words = message.Split(' ');
-
-				bool solution = true;
-				int count = 0;
-				foreach (String word in words)
-				{
-					if (!dictionary.Contains(word.ToLower()))
-					{
-						solution = false;
-						break;
-					}
-
-					if (++count > 4 && solution)
-						break;
-				}
-
-				if (solution)
-				{
-					Console.WriteLine("Found Solution:  {0}", message);
-					Console.Write("Key:  ");
-					for (int i = 0; i < key.Length; i++)
-					{
-						Console.Write("{0} ", key[i]);
-					}
-					Console.WriteLine();
-					mysolutions.Add((int[])key.Clone(), message);
-				}
-			});
-
-			foreach (int[] key in mysolutions.Keys)
-			{
-				if (!solutions.ContainsKey(key))
-				{
-					solutions.Add(key, mysolutions[key]);
-				}
-			}
-
-			Console.Write("Checked:  ");
-			for (int i = 0; i < akey.Length; i++)
-			{
-				Console.Write("{0} ", startingKey[i]);
-			}
-			Console.Write("to: ");
-			for (int i = 0; i < akey.Length; i++)
-			{
-				Console.Write("{0} ", akey[i]);
-			}
-			Console.WriteLine();
-		}
-
-		public Dictionary<int[], String> Crack2(String encryptedMessage, int keyLength)
 		{
 			if (keyLength <= 3) return VigenereCypher.Crack(encryptedMessage, keyLength);
 
@@ -194,6 +98,12 @@ namespace VigenereCypher
 			return solutions;
 		}
 
+		/// <summary>
+		/// Merges the dictionaries.
+		/// the second parameter is merged into the Target parameter.
+		/// </summary>
+		/// <param name="target">Target.</param>
+		/// <param name="toMerge">To merge.</param>
 		public void MergeDictionaries(ref Dictionary<int[], string> target, Dictionary<int[], string> toMerge)
 		{
 			foreach (int[] key in toMerge.Keys)
@@ -203,6 +113,11 @@ namespace VigenereCypher
 			}
 		}
 
+		/// <summary>
+		/// Check to see if the Key is all zero.
+		/// </summary>
+		/// <returns><c>true</c>, if key is all zeros, <c>false</c> otherwise.</returns>
+		/// <param name="key">Key.</param>
 		public bool KeyEqualsZero(int[] key)
 		{
 			foreach (int n in key)
@@ -213,6 +128,13 @@ namespace VigenereCypher
 			return true;
 		}
 
+
+		/// <summary>
+		/// Check to see if the given key is a solution to the problem.
+		/// </summary>
+		/// <returns>The check.</returns>
+		/// <param name="encryptedMessage">Encrypted message.</param>
+		/// <param name="key">Key.</param>
 		public Dictionary<int[], String> CrackCheck(String encryptedMessage, int[] key)
 		{
 			Dictionary<int[], String> mysolutions = new Dictionary<int[], string>();
